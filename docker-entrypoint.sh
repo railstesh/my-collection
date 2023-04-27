d73faedba63dccc3f8eefae92785b251a2136a76
railstesh <railsriteshc56@gmail.com>
2023-04-27 13:05:12 +0530

    add docker entrypoint for install missing gems and migrate panding migration

---
+++ docker-entrypoint.sh
@@ -0,0 +1,13 @@
+if [ "${RAILS_ENV}" = "production" ]; then
+  # Verify all the production gems are installed
+  bundle check
+  if [ ! -d "/data/public/uv" ]; then
+      bundle exec rake assets:clean assets:precompile
+  fi
+else
+  # install any missing development gems (as we can tweak the development container without rebuilding it)
+  bundle check || bundle install --without production
+fi
+ bundle exec rails db:create
+bundle exec rails db:migrate
+bundle exec rails s -p 3000 -b '0.0.0.0'
